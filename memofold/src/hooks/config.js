a864f855c91eb0789ca4f87246f79e259686a0e1
Nitin Kumar <106958535+nitink3006@users.noreply.github.com>
2025-07-11 12:37:28 +0530

    Updated Url (#13)

--- memofold/src/hooks/config.js
+++ memofold/src/hooks/config.js
@@ -1,5 +1,6 @@
 const config = {
-    apiUrl: "https://memofold1.onrender.com/api",
+    apiUrl: "https://memofold1.onrender.com/api", //Production API URL
+    // apiUrl: "http://localhost:5000/api", // Local development API URL
 };
 
 export default config;
