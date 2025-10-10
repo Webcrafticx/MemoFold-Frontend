63672d03c18f8fbac3d76026254a911d9b8d995e
Sarthak Rai <106958899+sarthak2685@users.noreply.github.com>
2025-10-10 17:36:26 +0530

    Update config.js

--- memofold/src/hooks/config.js
+++ memofold/src/hooks/config.js
@@ -1,6 +1,6 @@
 const config = {
-      // apiUrl: "https://memofold1.onrender.com/api", //Production API URL
-    apiUrl: "http://localhost:5000/api", 
+      apiUrl: "https://memofold1.onrender.com/api", //Production API URL
+    // apiUrl: "http://localhost:5000/api", 
 };
 
 export default config;
