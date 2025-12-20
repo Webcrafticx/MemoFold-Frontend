abb4f9608788cf6b360bdd910bde6467d57a4ac3
ErNikhil0 <nikhiln249btechaiml2025@kccitm.edu.in>
2025-12-20 17:06:50 +0530

    Update ProfilePostCard.jsx
    ..

--- memofold/src/components/profile/ProfilePostCard.jsx
+++ memofold/src/components/profile/ProfilePostCard.jsx
@@ -375,7 +375,7 @@ const ProfilePostCard = ({
                     />
                     <button
                         onClick={() =>
-                            onRemoveEditFile(editFiles.indexOf(file))
+                        onRemoveEditFile(editFiles.indexOf(file))
                         }
                         className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full p-1 text-xs cursor-pointer"
                     >
